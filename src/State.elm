d84c7f9f1917fae5ae8de644e8ffcad2282aa574
Thomas Dufour <td@thomasdufour.fr>
2017-01-11 00:14:16 +0100

    [passman-elm] Ugly form

---
+++ src/State.elm
@@ -0,0 +1,27 @@
+module State exposing (init, update)
+
+import Types exposing (..)
+
+init : (Model, Cmd msg)
+init = ({ key = "", masterPassword = "", variant = 1 }, Cmd.none)
+
+update : Msg -> Model -> (Model, Cmd msg)
+update msg =
+    case msg of
+        SetKey key -> setKey key
+        SetMasterPassword masterPassword -> setMasterPassword masterPassword
+        SetVariant variant -> setVariant variant
+
+setKey : String -> Model -> (Model, Cmd msg)
+setKey key model = ({ model | key = key }, Cmd.none)
+
+setMasterPassword : String -> Model -> (Model, Cmd msg)
+setMasterPassword masterPassword model = ({ model | masterPassword = masterPassword }, Cmd.none)
+
+setVariant : String -> Model -> (Model, Cmd msg)
+setVariant variant model = 
+    let model1 =
+        case String.toInt variant of
+            Ok v -> { model | variant = v }
+            _ -> model
+    in (model1, Cmd.none)
---
+++ src/Types.elm
@@ -0,0 +1,10 @@
+module Types exposing (..)
+
+type alias Model = 
+    { key: String
+    , masterPassword: String
+    , variant: Int }
+
+type Msg = SetKey            String
+         | SetMasterPassword String
+         | SetVariant        String
